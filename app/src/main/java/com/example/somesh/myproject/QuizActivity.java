bf6776ad676b74d5385b0970da192e121b84069a
Abhi <abhimanyu.n14@gmail.com>
2018-01-11 16:05:55 +0530

    Added scrolling

--- app/src/main/java/com/example/somesh/myproject/QuizActivity.java
+++ app/src/main/java/com/example/somesh/myproject/QuizActivity.java
@@ -2,12 +2,17 @@ package com.example.somesh.myproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
 
+    TextView question_tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        question_tv = findViewById(R.id.question_tv_id);
+        question_tv.setMovementMethod(new ScrollingMovementMethod());
     }
 }
